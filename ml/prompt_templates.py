61bc0d7ed9425ad4179750962f74ec82b8493648
mnbvcx111222333-sudo <mnbvcx111222333@gmail.com>
2025-09-28 19:02:28 +0300

    Create prompt_templates.py

---
+++ ml/prompt_templates.py
@@ -0,0 +1,13 @@
+GEN_RECEIPT = '''Here are the ingridients:
+{ingridients}
+
+Here is the receipt: {recept}
+Generate similar receipt for similar meal. Do NOT include the following: {forbidden_ingridients}.'''
+
+GEN_MEAL_PLAN = '''Generate a meal plan for {age_categoty} {weight_category} {gender_category} if their aim is {target}. Do NOT include meals with following ingridients: {forbidden_ingridients}.'''
+
+GEN_SHOPPING_LIST = '''Here is the meal plan:
+{meal_plan}
+Available products: {products}.
+Generate a shopping list.
+'''
