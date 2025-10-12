7d8b3264f67bb558f6f04ba3e78d75a3bfee2aab
mnbvcx111222333-sudo <mnbvcx111222333@gmail.com>
2025-10-12 14:26:36 +0300

    Update prompt_templates.py

--- ml/prompt_templates.py
+++ ml/prompt_templates.py
@@ -1,13 +1,23 @@
-GEN_RECEIPT = '''Here are the ingridients:
-{ingridients}
+RECEIPT_GEN_PROMPT = """
+Generate receipt for {meal}.
+Do NOT include the following products: {forbidden_products}.
+"""
+SHOPPING_LIST_PROMPT = """
+Here are the meals user wants to cook: {meals}.
+Create a shopping list for user, response in JSON format: ["product1", "product2", ...]
+Do NOT include the following products in the list: {forbidden_products}.
+Response with JSON-like list of items to buy: ["<item_1>", "<item_2", ..., etc.]
+"""
+MEAL_PLAN_PROMPT = """
+User wants to eat {target_calories} calorial food.
+Here are the products they would like to include in their meals: {available_products}. Here are the products you MUST NOT include even if user wants to: {forbidden_products},
+Create a detailed meal plan for a day, including breakfast, lunch, and dinner. Present the output strictly in the following valid JSON format, using lists for each meal type:
 
-Here is the receipt: {recept}
-Generate similar receipt for similar meal. Do NOT include the following: {forbidden_ingridients}.'''
+{{
+  "breakfast": ["<meal_1>", "<meal_2>", ...],
+  "lunch": ["<meal_3>"],
+  "dinner": ["<meal_4>"]
+}}
 
-GEN_MEAL_PLAN = '''Generate a meal plan for {age_categoty} {weight_category} {gender_category} if their aim is {target}. Do NOT include meals with following ingridients: {forbidden_ingridients}.'''
-
-GEN_SHOPPING_LIST = '''Here is the meal plan:
-{meal_plan}
-Available products: {products}.
-Generate a shopping list.
-'''
+Ensure that each list contains one or more meal items, describing typical dishes or ingredients for the meal. Each meal item should be a string. Do not include any explanations outside the JSON structure.
+"""
